4f443b79750c7d36689ba38526f3aeaa1e3b9715
Enio K S Meyer <enio.kaique@gmail.com>
2023-04-18 09:26:47 -0300

    Purificação

---
+++ Purificação/index.js
@@ -0,0 +1,10 @@
+let dadoPurificado = '';
+for (let caractere of stringCorrompida) {
+    if (caractere === '!' || caractere === '@' || caractere === '#' || caractere === '$' ||
+    caractere === '%' || caractere === '&' || caractere === '*' || caractere === '('||
+    caractere === ')' || caractere === '.') {
+        
+    } else {
+        dadoPurificado += caractere
+    }
+}  console.log(dadoPurificado)
